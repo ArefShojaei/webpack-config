5b7e4e7e3b16ed5d14cf622f434b21aef4c6bb28
ArefShojaei <arefshojaei82dev@gmail.com>
2023-06-08 20:47:52 +0330

    add webpack prod env config

---
+++ webpack.prod.js
@@ -0,0 +1,79 @@
+const path = require('path');
+const Webpackbar = require('webpackbar');
+const HtmlWebpack = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const SpeedMeasureWebpack = require('speed-measure-webpack-plugin');
+const MiniCssExtract = require('mini-css-extract-plugin');
+const WebpackBuildNotifierPlugin = require('webpack-build-notifier');
+
+module.exports = {
+    mode: "production",
+    entry: path.resolve(__dirname, "src/index.js"),
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "bundle.[contenthash].js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(woff2?|ttf|otf|eot)$/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        outputPath: "fonts",
+                        publicPath: "fonts",
+                        name: "[name].[contenthash].[ext]"
+                    }
+                }
+            },
+            {
+                test: /\.(png|jpe?g|svg)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            outputPath: "images",
+                            publicPath: "images",
+                            name: "[name].[contenthash].[ext]"
+                        }
+                    },
+                    "image-webpack-loader"
+                ]
+            },
+            {
+                test: /\.s?css$/,
+                use: [
+                    MiniCssExtract.loader,
+                    "css-loader",
+                    "sass-loader"
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env",]
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new Webpackbar({ name: "Processes :" }),
+        new SpeedMeasureWebpack(),
+        new CleanWebpackPlugin(),
+        new MiniCssExtract({ filename: "styles.[contenthash].css" }),
+        new HtmlWebpack({
+            filename: "index.html",
+            template: path.resolve(__dirname, "public/index.html"),
+            inject : "body"
+        }),
+        new WebpackBuildNotifierPlugin({
+            title: "Webpack compiler ( Production ENV )",
+            suppressSuccess: true,
+            showDuration : true,
+        }),
+    ]
+}
